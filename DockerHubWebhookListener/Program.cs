35845a88a62dd317ae56d9bbd93d0d261299bf7a
RobbeVanHemelryck <HNcvtd2vndtghDAeBGp552Wm4ehhLVgvAwtDTwQQsDCbGGv8kh7uJfGEbHXE6jF9>
2022-09-22 22:47:13 +0200

    Init

---
+++ DockerHubWebhookListener/Program.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var dockerComposeYml = Environment.GetEnvironmentVariable("docker-compose-yml");
+
+app.MapGet("/{repository}", (string repository) =>
+{
+    var cmd = new Process();
+    cmd.StartInfo.FileName = "cmd.exe";
+    cmd.StartInfo.RedirectStandardInput = true;
+    cmd.StartInfo.RedirectStandardOutput = true;
+    cmd.StartInfo.UseShellExecute = false;
+    cmd.StartInfo.CreateNoWindow = true;
+    cmd.Start();
+
+    cmd.StandardInput.WriteLine($"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}");
+    cmd.StandardInput.Flush();
+    cmd.StandardInput.Close();
+    cmd.WaitForExit();
+    Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+    return repository;
+});
+
+app.Run();

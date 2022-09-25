5b7d71965a7bbfea2d1a6c3ec177af56af67f962
RobbeVanHemelryck <HNcvtd2vndtghDAeBGp552Wm4ehhLVgvAwtDTwQQsDCbGGv8kh7uJfGEbHXE6jF9>
2022-09-25 23:14:22 +0200

    Listen to post's instead of get's

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ var app = builder.Build();
 
 var dockerComposeYml = Environment.GetEnvironmentVariable("docker-compose-yml");
 
-app.MapGet(
-    "/{repository}",
-    (string repository) => File.WriteAllText(
-        $"/commands/{Guid.NewGuid()}.txt",
-        $"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}"));
+app.MapPost("/{repository}", (string repository) =>
+{
+    File.WriteAllText(
+    $"/commands/{Guid.NewGuid()}.txt",
+    $"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}");
+});
 
 app.Run();

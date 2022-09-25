b783c34aa4c35ba59a55554fd300759dd20df4c6
RobbeVanHemelryck <HNcvtd2vndtghDAeBGp552Wm4ehhLVgvAwtDTwQQsDCbGGv8kh7uJfGEbHXE6jF9>
2022-09-25 18:39:11 +0200

    Refactor to only create file in commands directory, instead of executing the command

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
-using System.Diagnostics;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var dockerComposeYml = Environment.GetEnvironmentVariable("docker-compose-yml");
 
-app.MapGet("/{repository}", (string repository) =>
-{
-    var cmd = new Process();
-    cmd.StartInfo.FileName = "cmd.exe";
-    cmd.StartInfo.RedirectStandardInput = true;
-    cmd.StartInfo.RedirectStandardOutput = true;
-    cmd.StartInfo.UseShellExecute = false;
-    cmd.StartInfo.CreateNoWindow = true;
-    cmd.Start();
-
-    cmd.StandardInput.WriteLine($"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}");
-    cmd.StandardInput.Flush();
-    cmd.StandardInput.Close();
-    cmd.WaitForExit();
-    Console.WriteLine(cmd.StandardOutput.ReadToEnd());
-    return repository;
-});
+app.MapGet(
+    "/{repository}",
+    (string repository) => File.WriteAllText(
+        $"/commands/{Guid.NewGuid()}.txt",
+        $"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}"));
 
 app.Run();

f8384858b4e77f8129c9bb9178fb187a48ed6bda
RobbeVanHemelryck <HNcvtd2vndtghDAeBGp552Wm4ehhLVgvAwtDTwQQsDCbGGv8kh7uJfGEbHXE6jF9>
2022-09-25 23:30:43 +0200

    Pull latest version upon trigger

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ app.MapPost("/{repository}", (string repository) =>
 {
     File.WriteAllText(
     $"/commands/{Guid.NewGuid()}.txt",
-    $"docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}");
+    $"docker pull taltiko/{repository} && docker-compose -f {dockerComposeYml} up -d  --force-recreate {repository}");
 });
 
 app.Run();
